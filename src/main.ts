bc511cdbc1f13e98e9aa723a843d3d02a59eaf55
Tim <tim@Tims-MacBook-Pro.local>
2022-06-10 16:10:16 +0200

    fixed await on top level error and created dir docs for github pages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import "./style.css";
-import init, { add } from "../audio-analyzer-lib/pkg/audio_analyzer_lib.js";
-
-await init();
-console.log(add(4, 1));
+import init from "../audio-analyzer-lib/pkg/audio_analyzer_lib.js";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
@@ -10,3 +7,8 @@ app.innerHTML = `
   <h1 class="bg-red-400">Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
+
+// Initialize WebAssembly functionality.
+init().then((exports) => {
+  console.log(exports.add(4, 1));
+})
